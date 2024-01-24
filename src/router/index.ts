fcde3d63bb1f44309ca926b831140a9e01a92c30
Felisberto3 <felisbertoalberto183@gmail.com>
2024-01-24 23:19:42 +0100

    routes to comment

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import { municipioRouter } from "./municipio.routes";
 import { administrationsRouter } from "./administrations.routes";
 import { adminRouter } from "./admin.routes";
 import { postRouter } from "./post.routes";
+import { commentRouter } from "./comment.routes";
 
 const router = Router()
 
@@ -14,5 +15,6 @@ router.use('/api',municipioRouter)
 router.use('/api',administrationsRouter)
 router.use('/api',adminRouter)
 router.use('/api',postRouter)
+router.use('/api',commentRouter)
 
 export { router }
